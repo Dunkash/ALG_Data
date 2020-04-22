341209203b1aed43f9da1e78b299578b165fb4eb
Dunkash <45978367+Dunkash@users.noreply.github.com>
2020-04-22 16:06:39 +0300

    Initial commit

---
+++ ALG_Word/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Data;
+
+namespace ALG_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input path to the txt file, containing rules");
+            Console.WriteLine("Write EXIT any time to exit application");
+            var input = "";
+            while (!File.Exists(input))
+            {
+                input = Console.ReadLine();
+                if (input == "EXIT")
+                    Environment.Exit(0);
+                if (!File.Exists(input))
+                    Console.WriteLine("Directory doesn't exist");
+            }
+            var rules = new Rulesheet(input);
+            
+            Console.WriteLine("\nAlphabet of this task:");
+            Console.Write("{");
+            for (var i=0;i<rules.alphabet.Length;i++)
+            {
+                Console.Write(rules.alphabet[i]);
+                Console.Write(";");
+            }
+            Console.Write("}\n");
+            Console.Write("\n");
+
+            Console.WriteLine("Rules of this task:");
+            foreach(var i in rules.Rules)
+            {
+                Console.Write(i.Key + "=>");
+                if (i.Value.Second)
+                    Console.Write(".");
+                Console.Write(i.Value.First+";\n");
+            }
+            Console.Write("\n");
+
+            while (input!="EXIT")
+            {
+                here: Console.WriteLine("Write word to apply substitutions. Write \"EXIT\" to end session. Write \"CAP\" to set iteration cap ");
+                input = Console.ReadLine();
+                Console.WriteLine();
+                if (input=="CAP")
+                {
+                    Console.WriteLine("Input maximum number of iterations");
+                    var cap = Console.ReadLine();
+                    int newcap=0;
+                    if (int.TryParse(cap, out newcap))
+                        rules.IterationCap = newcap;
+                    else
+                        Console.WriteLine("Incorrect input");
+                    goto here;
+                }
+                foreach (var i in input)
+                {
+                    if (!rules.alphabet.ToList().Contains(Char.ToString(i)))
+                    { 
+                        Console.WriteLine("Word contains symbols, not supported by alphabet \n");
+                        goto here;
+                    }
+
+                }
+                rules.Apply(input);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+/*
+ var rules = new Dictionary<string, Pair<string, bool>>
+            {
+                {"a",new Pair<string,bool>{First="bb",Second = false } },
+                {"b",new Pair<string,bool>{First="E",Second = false }}
+            };
+*/
+
---
+++ ALG_Word/Rulesheet.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace ALG_Word
+{
+
+    public class Pair<T, K>
+    {
+        public T First { get; set; }
+        public K Second { get; set; }
+    }
+
+    class Rulesheet
+    {
+        public string ReplaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+
+        public int IterationCap = 1000;
+        public string[] alphabet;
+        public Dictionary<string, Pair<string, bool>> Rules;
+
+        public Rulesheet(Dictionary<string, Pair<string, bool>> r)
+        {
+            Rules = r;
+            if (Rules.Keys.Contains("E"))
+            {
+                var temp = Rules["E"];
+                Rules.Remove("E");
+                Rules["\b"] = temp;
+            }
+
+        }
+
+        public Rulesheet(string file)
+        {
+            Rules = new Dictionary<string, Pair<string, bool>>();
+
+            var lines = File.ReadAllLines(file);
+            alphabet = lines[0].Split(';');
+            for (int i=1; i<lines.Length;i++)
+            {
+                var temp = lines[i].Split('>');
+                string from = temp[0];
+                bool stop = (temp[1].First() == '.');
+                string to;
+                if (stop)
+                    to = temp[1].Substring(1);
+                else
+                    to = temp[1];
+                Rules.Add(from,new Pair<string, bool> {First=to,Second=stop});
+            }
+            if (Rules.Keys.Contains("E"))
+            {
+                var temp = Rules["E"];
+                Rules.Remove("E");
+                Rules[""] = temp;
+            }
+        }
+        public string Apply(string s)
+        {
+            Console.Write(s);
+            int iteration = 0;
+            start: while (true)
+            {
+                foreach (var rule in Rules)
+                {
+                    if (s.IndexOf(rule.Key)!=-1)
+                    {
+                        if (rule.Key == "")
+                            s = rule.Value.First + s;
+                        else if (rule.Value.First != "E")
+                            s = ReplaceFirst(s, rule.Key, rule.Value.First);
+                        else s = ReplaceFirst(s, rule.Key, "");
+
+                        if (s != "") Console.Write("=>" + s);
+                        else Console.Write("=>" + "E");
+                            
+                        if (rule.Value.Second)
+                        {
+                            Console.Write(";");
+                            return s;
+                        }
+                        iteration++;
+                        if (iteration > IterationCap)
+                        { 
+                            Console.Write(";\nOperations cap reached. Ending execution.");
+                            return "";
+                        }
+
+                        goto start;
+                    }
+                }
+                Console.Write(";");
+                return s;
+            }
+        }
+    }
+}
